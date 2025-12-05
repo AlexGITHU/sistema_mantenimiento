3be6eadd7b0782d77d4929f60fa7eb0eeb870d62
AlexGITHU <86991278+AlexGITHU@users.noreply.github.com>
2025-12-05 17:15:10 -0600

    Add files via upload

---
+++ inicio.bat
@@ -0,0 +1,47 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJEGF+VIgFBNASAuBL1epD6YI7eX60OeGrFkYWusycYrVzruyKeMc5EqqfJUitg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJEGF+VIgFBNASAuBL1epD6YI7eX60OeGrFkYWusycYrVzruyE+kA/kDpdNYA13ROncUFARJdcAakIAosrA4=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Iniciando Sistema de Mantenimiento...
+
+REM Inicia Apache y MySQL como servicios (en segundo plano)
+net start Apache2.4
+net start MySQL
+
+REM Espera unos segundos
+timeout /t 3 /nobreak > NUL
+
+REM Abre el navegador con tu sistema
+start "" "http://localhost/sistema_mantenimiento/index.php/"
+
+exit
