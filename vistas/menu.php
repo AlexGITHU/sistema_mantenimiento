c7ab97bbc853b67c9064c8b7fda608cbedc4b968
AlexGITHU <86991278+AlexGITHU@users.noreply.github.com>
2025-12-05 20:15:26 -0600

    Update menu.php

--- vistas/menu.php
+++ vistas/menu.php
@@ -1,4 +1,4 @@
-﻿<?php require_once "dependencias.php";
+<?php require_once "dependencias.php";
   function esAdministrador() {
   return isset($_SESSION['rol']) && $_SESSION['rol'] != 'Administrador';
 }
@@ -27,12 +27,34 @@
           <a class="nav-link" href="usuarios.php">Usuarios</a>
         </li>
         <?php endif; ?>
-        <li class="nav-item">
-          <a class="nav-link" href="inventario.php">Inventario</a>
+
+
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span> Administrar Inventario <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="inventario.php">Inventario</a></li>
+              <li><a class="dropdown-item" href="proveedores.php">Lista de Proovedores</a></li>
+            </ul>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="tareas.php">Tareas</a>
         </li>
+        <li>
+          <a class="nav-link" href="estado.php">Estado Articulos</a>
+        </li>
+<!--         <li class="nav-item">
+          <a class="nav-link" href="quejas.php">Quejas</a>
+        </li> -->
+
+        <li>
+          <a class="nav-link" href="generadorQR.php">Generador QR</a>
+        </li>
+
+        <li>
+          <a class="nav-link" href="valesGen.php">Generador de Vales</a>
+        </li>
+        
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-danger" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Usuario: <?php echo $_SESSION['usuario']; ?>
